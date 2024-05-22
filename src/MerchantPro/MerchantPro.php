db685c22a981f67c1b8e0b63a1f9996df13f6cdd
Shu Saura <35106430+shusaura85@users.noreply.github.com>
2024-05-22 12:19:19 +0300

    Added customers endpoint to constants

--- src/MerchantPro/MerchantPro.php
+++ src/MerchantPro/MerchantPro.php
@@ -20,11 +20,12 @@ class MerchantPro
 	protected $verifyPeer = true;
 	
 	
-	const MP_BATCH		= '/api/v2/batch';			// used with POST request. Send up to 20 requests in one batch
+	const MP_BATCH      = '/api/v2/batch';			// used with POST request. Send up to 20 requests in one batch
 	const MP_PRODUCTS   = '/api/v2/products';		// add /{id} to load specific product
 	const MP_CATEGORIES = '/api/v2/categories';		// add /{id} to load specific category
 	const MP_INVENTORY  = '/api/v2/inventory';		// add /{type}/{id} to load specific item by type (ex: /id/1234 /ean/5200000000001 /sku/VTR345)
 	const MP_ORDERS     = '/api/v2/orders';			// add /{id} to load specific order
+	const MP_CUSTOMERS  = '/api/v2/customers';		// add /{id} to load specific customer
 	
 	public function __construct($storeUrl, $apiKey, $apiSecret)
 		{
